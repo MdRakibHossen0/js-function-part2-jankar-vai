ff0768840484bfbed73c0e6d5062461719e01c64
MdRakibHossen0 <mdrakibhossencse@gmail.com>
2025-02-17 01:53:13 +0600

    added total cost

--- cheapest.js
+++ cheapest.js
@@ -8,17 +8,15 @@ const phones = [
 
 function getCheapestPhone(phones) {
   let min = phones[0];
-  for(const phone of phones){
-   if(phone.price < min.price){
-    min = phone;
-   }
-
+  for (const phone of phones) {
+    if (phone.price < min.price) {
+      min = phone;
+    }
   }
   return min;
 }
 const cheap = getCheapestPhone(phones);
 console.log("cheapest phone is: ", cheap);
-
 
 //H.w task jhankar vai
 
@@ -27,22 +25,18 @@ const mobiles = [
   { name: "xoami", price: 2000, camera: "12mp", color: "black" },
   { name: "oppo", price: 12000, camera: "12mp", color: "black" },
   { name: "walton", price: 9000, camera: "12mp", color: "black" },
-  { name: "Iphone", price: 120000, camera: "12mp", color: "titanium black"},
+  { name: "Iphone", price: 120000, camera: "12mp", color: "titanium black" },
 ];
 
-function getMobile(){
-    let max = mobiles[0]; 
+function getMobile() {
+  let max = mobiles[0];
 
-    for(const mobile of mobiles){
-
-        if(mobile.price > max.price ){
-            max = mobile;
-        }
-
+  for (const mobile of mobiles) {
+    if (mobile.price > max.price) {
+      max = mobile;
     }
-    return max;
-
+  }
+  return max;
 }
-const highPrice= getMobile();
-console.log('Height price mobile:',highPrice)
-
+const highPrice = getMobile();
+console.log("Height price mobile:", highPrice);
